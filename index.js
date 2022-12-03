3cf800dafb201187b26564c9de752381cf52839b
404: CTO_not_found <ebukauche52@gmail.com>
2022-12-03 20:40:09 +0100

    backend interview test - 3rd Dec 22

---
+++ index.js
@@ -0,0 +1,21 @@
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets  (boths uppercase and lowercase) and numeric digits.
+
+// Exampples 
+// "abcde" => 0 no caracter repeats more than one.
+// "aabbcde" => 2 a and b
+// "aabBcde" => 2 a and b
+// "invisibility" => 1 i occurs 6 times
+// "aa11" => 2 a and 1
+// "ABBA" => 2 a and b
+
+function countDuplicates(strOfData) {
+    let stringArr = strOfData.split('');
+    let total = {};
+    
+    stringArr.forEach(e => {
+        total[e] = (total[e] || 0) + 1;
+    });
+    return total;
+}
+
+console.log(countDuplicates("aabbcde"));
